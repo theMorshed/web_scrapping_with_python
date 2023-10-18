2e54a32ccc9aec2a1d216fc6e40cb95594f60d01
theMorshed <themorshedctg@gmail.com>
2023-10-18 22:43:06 +0600

    update code with function api

--- project_one/timesjobs_scrapper.py
+++ project_one/timesjobs_scrapper.py
@@ -1,19 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
+import time
 
-html_file = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
-soup = BeautifulSoup(html_file, 'lxml')
-jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+print('Put some skills that are not familiar with')
+unfamiliar_skill = input('> ')
+print(f'Filtering out {unfamiliar_skill}')
 
-for job in jobs:
-    published_date = job.find('span', class_='sim-posted').span.text
-    if 'few' in published_date:
-        continue
-    company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
-    skills = job.find('span', class_='srp-skills').text.replace(' ', '')
-    job_link = job.find('div', class_='applied-dtl clearfix').a['href']
-    
-    print(f'Company Name: {company_name.strip()}')
-    print(f'Skills: {skills.strip()}')
-    print(f'More Info: {job_link}')
+def find_jobs():
+    html_file = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+    soup = BeautifulSoup(html_file, 'lxml')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+
+    for job in jobs:
+        published_date = job.find('span', class_='sim-posted').span.text
+        if 'few' in published_date:
+            continue
+        company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+        skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+        job_link = job.header.h2.a['href']
+        
+        if unfamiliar_skill in skills:
+            continue
+        
+        print(f'Company Name: {company_name.strip()}')
+        print(f'Skills: {skills.strip()}')
+        print(f'More Info: {job_link}\n')
+        
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting for {time_wait} minutes...')
+        time.sleep(time_wait * 60)
     
