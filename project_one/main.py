a4f47aaef68cdd351b95e06fe09559144a7bb249
theMorshed <themorshedctg@gmail.com>
2023-10-18 12:26:39 +0600

    get all course title with find and find_all method of beautifulsoap

--- project_one/main.py
+++ project_one/main.py
@@ -2,4 +2,10 @@ from bs4 import BeautifulSoup
 
 with open('home.html', 'r') as html_file:
     content = html_file.read()
-    print(content)
+    
+    soup = BeautifulSoup(content, 'lxml')
+    # print(soup.prettify())
+    # tags = soup.find('h5') # it will get you first match
+    courses_tags = soup.find_all('h5')
+    for course in courses_tags:
+        print(course.text)
