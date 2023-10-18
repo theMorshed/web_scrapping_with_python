a766c4874bbbc89827e65b8205546853e3a639fb
theMorshed <themorshedctg@gmail.com>
2023-10-18 12:39:06 +0600

    home.html scrapper finished temporarily

--- project_one/main.py
+++ project_one/main.py
@@ -6,6 +6,12 @@ with open('home.html', 'r') as html_file:
     soup = BeautifulSoup(content, 'lxml')
     # print(soup.prettify())
     # tags = soup.find('h5') # it will get you first match
-    courses_tags = soup.find_all('h5')
-    for course in courses_tags:
-        print(course.text)
+    # courses_tags = soup.find_all('h5')
+    # for course in courses_tags:
+    #     print(course.text)
+    course_containers = soup.find_all('div', class_= 'card')
+    for course in course_containers:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+        
+        print(f'{course_name} costs {course_price}')
