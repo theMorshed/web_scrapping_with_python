7667af2bf2f7aac4a73170ac8a3e9790aa2fe0c3
theMorshed <themorshedctg@gmail.com>
2023-10-27 22:46:12 +0600

    added next page follow method

--- scrapy2.11.0_Documentation/scrapyTutorial/tutorial/tutorial/spiders/quotes_spider.py
+++ scrapy2.11.0_Documentation/scrapyTutorial/tutorial/tutorial/spiders/quotes_spider.py
@@ -5,7 +5,6 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         "https://quotes.toscrape.com/page/1/",
-        "https://quotes.toscrape.come/page/2/",
     ]
             
     def parse(self, response):
@@ -15,3 +14,7 @@ class QuotesSpider(scrapy.Spider):
                 "author": quote.css("small.author::text").get(),
                 "tags": quote.css("div.tags a.tag::text").getall(),
             }
+            
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
